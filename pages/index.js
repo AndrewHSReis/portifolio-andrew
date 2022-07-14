c2dbbcdc0856cc4c02e08b4b26f9301f1798458b
unknown <andrewhend2015@gmail.com>
2022-07-14 11:12:16 -0400

    portifolio andrew

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,37 @@
-function Home() {
-    return <div>Home</div>
+function CapsLock(props) {
+    const testoInserido = props.children;
+    const testoEmCapsLock = testoInserido.toUpperCase();
+    return <div>{testoEmCapsLock}</div>
 }
-export default Home
+import { setState } from 'react';
+import { useState } from 'react';
+
+function Contador(){
+    const [contador,setContador] = useState(0);
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+
+    function subtrairContador(){
+        setContador(contador - 1);
+    }
+
+    return (
+        <div>
+            <h3><CapsLock>Contador Teste</CapsLock></h3>
+            <div>
+                <div>Resultado = {contador}</div>
+                <button onClick={adicionarContador}><CapsLock>Adicionar</CapsLock></button>
+                <button onClick={subtrairContador}><CapsLock>Subitrair</CapsLock></button>
+            </div>
+        </div>
+    )
+}
+
+function Pagina() {
+    //return <CapsLock>Me deixe em CapsLock por favor</CapsLock>
+    return <Contador/>
+}
+
+export default Pagina
